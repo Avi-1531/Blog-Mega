8b2679ecbf916a5a9b8e70eab7cbbdc57ffef712
Avi Chauhan <avichauhan1531@gmail.com>
2023-10-07 22:19:49 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
 import {
+  HashRouter,
   Route,
   RouterProvider,
   createBrowserRouter,
@@ -71,9 +72,11 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <HashRouter>
+        <RouterProvider router={router}>
+          <App />
+        </RouterProvider>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
